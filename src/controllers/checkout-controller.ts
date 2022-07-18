526c6838e9d9e17fea47933706744b8787d1633e
foxyuh <arthurbryansk@gmail.com>
2022-07-18 19:16:47 -0300

    Update checkout-controller.ts

--- src/controllers/checkout-controller.ts
+++ src/controllers/checkout-controller.ts
@@ -8,96 +8,107 @@ import { EventEmitter } from "../utils/event-emitter.js";
 export { CheckoutController };
 
 class CheckoutController {
+  $ = document.querySelector.bind(document);
+  email = this.$("#email");
+  nome = this.$("#nome");
+  sobrenome = this.$("#sobrenome");
+  endereço = this.$("#endereco");
+  endereço2 = this.$("#endereco2");
+  complemento = this.$("#complemento");
+  codigoPostal = this.$("#codigo-postal");
+  telefone = this.$("#telefone");
+  cidade = this.$("#cidade");
+  estado = this.$("#estado");
 
-    $ = document.querySelector.bind(document)
-    email = this.$('#email')
-    nome = this.$('#nome')
-    sobrenome = this.$('#sobrenome')
-    endereço = this.$('#endereco')
-    endereço2 = this.$('#endereco2')
-    complemento = this.$('#complemento')
-    codigoPostal = this.$('#codigo-postal')
-    telefone = this.$('#telefone')
-    cidade = this.$('#cidade')
-    estado = this.$('#estado')
+  pedidoLog = new PedidoLog();
+  produtos = new Produtos();
+  valorEntrega = "20,10";
+  metodoDeEntrega = [];
 
-    pedidoLog = new PedidoLog();
-    produtos = new Produtos()
-    valorEntrega = '20,10';
-    metodoDeEntrega = []
+  constructor() {
+    this.init();
+  }
 
+  init() {
+    this.produtos.adiciona(
+      new Produto(
+        "Playstation 5",
+        "Preto/Branco",
+        "30cm",
+        1,
+        "4.399,90",
+        "../src/assets/imgs/ps5.png",
+        500
+      ),
+      this.produtos
+    );
 
-    constructor() {
-        this.init();
-    };
+    this.produtos.adiciona(
+      new Produto(
+        "Pulse 3D",
+        "Preto/Branco",
+        "15cm",
+        2,
+        "479,90",
+        "../src/assets/imgs/pulse3d.png",
+        437
+      ),
+      this.produtos
+    );
 
-    init () {
+    // console.log(this.produtos)
+    this.atualizaInformacoes(this.valorEntrega, this.produtos);
+  }
 
-        this.produtos.adiciona(new Produto(
-            'Playstation 5', 
-            'Preto/Branco', 
-            '30cm', 
-            1, 
-            '4.399,90', 
-            '../src/assets/imgs/ps5.png', 
-            500
-            ), this.produtos)
+  adiciona() {
+    document.querySelector(".entrega").classList.add("display-none");
+    document.querySelector(".pagamento").classList.remove("display-none");
 
-            this.produtos.adiciona(new Produto(
-                'Pulse 3D', 
-                'Preto/Branco', 
-                '15cm', 
-                2, 
-                '479,90', 
-                '../src/assets/imgs/pulse3d.png', 
-                437
-                ), this.produtos)
+    const entrega = new Entrega(
+      this.email,
+      this.nome,
+      this.sobrenome,
+      this.endereço,
+      this.endereço2,
+      this.complemento,
+      this.codigoPostal,
+      this.telefone,
+      this.cidade,
+      this.estado
+    );
+    this.pegaMetodoDeEntrega();
+    const metodoDeEntrega = new MetodoDeEntrega(this.metodoDeEntrega);
 
-                // console.log(this.produtos)
-        this.atualizaInformacoes(this.valorEntrega, this.produtos)
-    };
+    this.pedidoLog.adiciona(entrega, metodoDeEntrega);
+  }
 
-    adiciona() {
+  pegaMetodoDeEntrega() {
+    document.querySelectorAll(".td").forEach((element) => {
+      element.childNodes.forEach((element) => {
+        const elemento: any = element;
+        if (element.nodeName == "INPUT") {
+          if (elemento.checked) {
+            const elementoParent: HTMLElement = elemento.parentNode.parentNode;
+            const tipoDeEntrega: any =
+              elementoParent.querySelector(".p-tipo-de-entrega");
+            const dias: any = elementoParent.querySelector(".dias");
+            const valor: any = elementoParent.querySelector(".number");
 
-        document.querySelector('.entrega').classList.add('display-none')
-        document.querySelector('.pagamento').classList.remove('display-none')
+            this.metodoDeEntrega = [
+              tipoDeEntrega.innerText,
+              dias.innerText,
+              valor.innerText,
+            ];
+          }
+        }
+      });
+    });
+  }
 
-        const entrega = new Entrega(this.email, this.nome, this.sobrenome, this.endereço, this.endereço2, this.complemento, this.codigoPostal, this.telefone, this.cidade, this.estado);
-        this.pegaMetodoDeEntrega();
-        const metodoDeEntrega = new MetodoDeEntrega(this.metodoDeEntrega);
-
-        this.pedidoLog.adiciona(entrega, metodoDeEntrega);
-    };
-
-    pegaMetodoDeEntrega() {
-
-        document.querySelectorAll('.td').forEach(element => {
-    
-            element.childNodes.forEach(element => {
-
-                const elemento: any = element;
-                if ( element.nodeName == 'INPUT' ) {
-                    if ( elemento.checked ) {
-                        const elementoParent: HTMLElement = elemento.parentNode.parentNode;
-                        const tipoDeEntrega: any = elementoParent.querySelector('.p-tipo-de-entrega');
-                        const dias: any = elementoParent.querySelector('.dias');
-                        const valor: any = elementoParent.querySelector('.number');
-    
-                        this.metodoDeEntrega = [tipoDeEntrega.innerText, dias.innerText, valor.innerText];
-                    };
-                };
-            });
-        });
-    };
-
-    atualizaInformacoes(envio, produtos, codigo=0) {
-
-        EventEmitter.emit('Exibe-Produtos', produtos)
-        EventEmitter.emit('Info-Itens', produtos)
-        EventEmitter.emit('Info-Envio', envio)
-        EventEmitter.emit('Info-Total', [envio, produtos, codigo])
-
-    }
-
-
-};
+  atualizaInformacoes(envio, produtos, codigo = 0) {
+    EventEmitter.emit("Exibe-Produtos", produtos);
+    EventEmitter.emit("Info-Itens", produtos);
+    EventEmitter.emit("Info-Envio", envio);
+    EventEmitter.emit("Info-Total", [envio, produtos, codigo]);
+  }
+}
