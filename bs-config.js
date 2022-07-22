f0ac5e92aea1e68beef679f4747b2197958ef1ab
foxyuh <arthurbryansk@gmail.com>
2022-07-22 13:43:29 -0300

    Revert "Update bs-config.js"
    This reverts commit 2326d6db0e4fd01b0e820e2f587e84fc33e373e2.

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,14 @@
-module.exports = {
-  port: process.env.PORT,
-  files: ['./**/*.{html,htm,css,js}'],
-  server:{
-      baseDir: "./"
-  }
-};
+module.exports = function (bs) {
+    return {
+      port: 3000,
+      files: ["./src/**/*.{html,htm,css,js}"],
+      server: {
+        middleware: {
+          1: require('connect-history-api-fallback')({
+            index: './public/index.html',
+            verbose: true,
+          }),
+        },
+      },
+    };
+  };
